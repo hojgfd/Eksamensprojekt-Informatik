092fcd10c58892941c89337c659bb437f90aadb8
Oscar <oscaremil.seiersen@gmail.com>
2026-03-23 13:07:18 +0100

    correct path

--- server/flask_app.py
+++ server/flask_app.py
@@ -2,16 +2,15 @@ from flask import Flask, request
 import git
 app = Flask(__name__)
 @app.route('/update_server', methods=['POST'])
-    def webhook():
-        if request.method == 'POST':
-            repo = git.Repo('path/to/git_repo')
-            origin = repo.remotes.origin
-origin.pull()
-return 'Updated PythonAnywhere successfully', 200
-        else:
-            return 'Wrong event type', 400
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/oscar1234/Eksamensprojekt-Informatik')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 @app.route('/')
 def hello_world():
     return 'Hello from Flask!'
-
