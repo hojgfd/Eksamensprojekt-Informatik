c30ab359a9b3204f485e7229c16055872fca9108
Agchr <agc@aktivit.dk>
2026-03-25 10:30:43 +0100

    reloader app nu (forhåbenligt)

--- server/flask_app.py
+++ server/flask_app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 @app.route('/update_server')
 def update():
     os.system('cd /home/oscar1234/Eksamensprojekt-Informatik && git pull')
-    return 'Updated'
+    # Til at reload app
+    os.system("touch /var/www/oscar1234_pythonanywhere_com_wsgi.py")
+    return 'Updated and reloaded'
 
 @app.route('/')
 def hello_world():
