a3f282e869dc8c5e5713357faed8f0672180974c
Oscar <oscaremil.seiersen@gmail.com>
2026-03-25 14:24:35 +0100

    database fix for serverz

--- server/database.py
+++ server/database.py
@@ -1,7 +1,11 @@
 import sqlite3
+import os
 
 def get_db():
-    conn = sqlite3.connect("parking.db")
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "parking.db")
+
+    conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
 
