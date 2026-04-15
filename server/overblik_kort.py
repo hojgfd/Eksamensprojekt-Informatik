69becba6908838a74c724cadd33da1ef17e317db
LukasLKT <lukaskt2006@gmail.com>
2026-04-15 11:26:15 +0200

    overblik over reseverede pladser med kort over parkeringsplads

--- server/overblik_kort.py
+++ server/overblik_kort.py
@@ -1,22 +1,75 @@
-import plotly.express as px
+import plotly.graph_objects as go
 from PIL import Image
 from jinja2 import Template
 
-image = Image.open("Images/Overblik.jpg")
+image = Image.open("static/Overblik.jpg")
 
-fig = px.scatter_geo(
-    lat=[56.152880],
-    lon=[10.190033],
-    hover_name=["Denmark"],
-    title="Specific Place on Map",
-    scope="europe",
-)
-fig.add_layout_image(source=image)
+# Create figure
+fig = go.Figure()
 
-fig.update_geos(
-    scope="europe",
-    projection_type="orthographic"
+# Constants
+img_width = 1600
+img_height = 900
+scale_factor = 0.5
+
+# Add invisible scatter trace.
+# This trace is added to help the autoresize logic work.
+fig.add_trace(
+    go.Scatter(
+        x=[0, img_width * scale_factor],
+        y=[0, img_height * scale_factor],
+        mode="markers",
+        marker_opacity=0
+    )
 )
+
+# Configure axes
+fig.update_xaxes(
+    visible=False,
+    range=[0, img_width * scale_factor]
+)
+
+fig.update_yaxes(
+    visible=False,
+    range=[0, img_height * scale_factor],
+    # the scaleanchor attribute ensures that the aspect ratio stays constant
+    scaleanchor="x"
+)
+
+# Add image
+fig.add_layout_image(
+    dict(
+        x=0,
+        sizex=img_width * scale_factor,
+        y=img_height * scale_factor,
+        sizey=img_height * scale_factor,
+        xref="x",
+        yref="y",
+        opacity=1.0,
+        layer="below",
+        sizing="stretch",
+        source=image)
+)
+
+fig.add_trace(go.Scatter(
+    x=[100, 150, 200, 250],
+    y=[300, 300, 300, 300],
+    mode='markers',
+    marker=dict(
+        color='rgb(255,0,0)',
+    ),
+    name='Reserveret'
+))
+
+fig.add_trace(go.Scatter(
+    x=[100, 150, 200, 250],
+    y=[50, 50, 50, 50],
+    mode='markers',
+    marker=dict(
+        color='rgb(255,0,255)',
+    ),
+    name='Kan Ikke Reserveres'
+))
 
 fig.show()
 
